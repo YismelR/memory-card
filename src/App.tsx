f928e2c9ef19103e20eb5dae18b5354d643a1522
YismelR <yismel_rodriguez@outlook.com>
2024-09-11 13:09:09 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+
+import PokeMemoryCard from "./PokeMemoryCard";
+
+function App() {
+    // const [count, setCount] = useState(0)
+
+    return <PokeMemoryCard />;
+}
+
+export default App;
