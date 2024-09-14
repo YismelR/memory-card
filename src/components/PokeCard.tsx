b0fe87d370fc9a9ae7f77588a03dc766e4aa9086
YismelR <yismel_rodriguez@outlook.com>
2024-09-13 21:59:12 -0400

    placed the setpokemon in a useeffect

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -1,48 +1,53 @@
 import usePokemon from "@/hooks/usePokemon";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { PokeData } from "@/hooks/usePokemon";
+import { useEffect } from "react";
 
 type PokeCardProps = {
-    setScore: React.Dispatch<React.SetStateAction<number>>;
+  setScore: React.Dispatch<React.SetStateAction<number>>;
 };
 export default function PokeCard({ setScore }: PokeCardProps) {
-    const { pokemons, setPokemons } = usePokemon();
+  const { pokemons, setPokemons } = usePokemon();
 
-    const shuffleCards = (pokemons: PokeData[]) => {
-        const cardsCopy = [...pokemons];
+  const shuffleCards = (pokemons: PokeData[]) => {
+    const cardsCopy = [...pokemons];
 
-        for (let i = cardsCopy.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [cardsCopy[i], cardsCopy[j]] = [cardsCopy[j], cardsCopy[i]];
-        }
-        return cardsCopy;
-    };
+    for (let i = cardsCopy.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [cardsCopy[i], cardsCopy[j]] = [cardsCopy[j], cardsCopy[i]];
+    }
+    return cardsCopy;
+  };
 
-    const handleClick = () => {
-        setScore((score: number) => score + 1);
-        setPokemons(shuffleCards(pokemons));
-    };
+  const handleClick = () => {
+    setScore((score: number) => score + 1);
+    setPokemons(shuffleCards(pokemons));
+  };
 
-    return (
-        <section className="grid gap-4 min-[520px]:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mt-6">
-            {pokemons.map((pokemon, idx) => (
-                <div
-                    onClick={handleClick}
-                    key={idx}
-                    className="hover:shadow-hovercard from-indigo-500 from-10% via-sky-500 via-30% to-yellow-300 to-50% p-1 rounded-2xl cursor-pointer animated-gradient hover:bg-gradient-to-r"
-                >
-                    <Card className="shadow-2xl">
-                        <CardContent>
-                            <img src={pokemon.src} />
-                        </CardContent>
-                        <CardHeader>
-                            <CardTitle className=" text-xl md:text-2xl">
-                                {pokemon.name}
-                            </CardTitle>
-                        </CardHeader>
-                    </Card>
-                </div>
-            ))}
-        </section>
-    );
+  useEffect(() => {
+    setPokemons(shuffleCards(pokemons));
+  }, []);
+
+  return (
+    <section className="grid gap-4 min-[520px]:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mt-6">
+      {pokemons.map((pokemon, idx) => (
+        <div
+          onClick={handleClick}
+          key={idx}
+          className="hover:shadow-hovercard from-indigo-500 from-10% via-sky-500 via-30% to-yellow-300 to-50% p-1 rounded-2xl cursor-pointer animated-gradient hover:bg-gradient-to-r"
+        >
+          <Card className="shadow-2xl">
+            <CardContent>
+              <img src={pokemon.src} />
+            </CardContent>
+            <CardHeader>
+              <CardTitle className=" text-xl md:text-2xl">
+                {pokemon.name}
+              </CardTitle>
+            </CardHeader>
+          </Card>
+        </div>
+      ))}
+    </section>
+  );
 }
