c83315f047011f3ec26a426ccada4aff3d70f6a3
YismelR <yismel_rodriguez@outlook.com>
2024-09-13 22:41:15 -0400

    handled the event when the same card is clicked the count should reset to 0

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -1,13 +1,14 @@
 import usePokemon from "@/hooks/usePokemon";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { PokeData } from "@/hooks/usePokemon";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 type PokeCardProps = {
   setScore: React.Dispatch<React.SetStateAction<number>>;
 };
 export default function PokeCard({ setScore }: PokeCardProps) {
   const { pokemons, setPokemons } = usePokemon();
+  const [clickedPokemons, setClickedPokemons] = useState<string[]>([]);
 
   const shuffleCards = (pokemons: PokeData[]) => {
     const cardsCopy = [...pokemons];
@@ -19,8 +20,14 @@ export default function PokeCard({ setScore }: PokeCardProps) {
     return cardsCopy;
   };
 
-  const handleClick = () => {
-    setScore((score: number) => score + 1);
+  const handleClick = (name: string) => {
+    if (clickedPokemons.includes(name)) {
+      setScore(0);
+      setClickedPokemons([]);
+    } else {
+      setScore((score: number) => score + 1);
+      setClickedPokemons([...clickedPokemons, name]);
+    }
     setPokemons(shuffleCards(pokemons));
   };
 
@@ -30,10 +37,10 @@ export default function PokeCard({ setScore }: PokeCardProps) {
 
   return (
     <section className="grid gap-4 min-[520px]:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mt-6">
-      {pokemons.map((pokemon, idx) => (
+      {pokemons.map((pokemon) => (
         <div
-          onClick={handleClick}
-          key={idx}
+          onClick={() => handleClick(pokemon.name)}
+          key={pokemon.name}
           className="hover:shadow-hovercard from-indigo-500 from-10% via-sky-500 via-30% to-yellow-300 to-50% p-1 rounded-2xl cursor-pointer animated-gradient hover:bg-gradient-to-r"
         >
           <Card className="shadow-2xl">
--- src/components/Scores.tsx
+++ src/components/Scores.tsx
@@ -1,21 +1,18 @@
 import { useState } from "react";
 
 type ScoresProps = {
-    score: number;
+  score: number;
 };
 export default function Scores({ score }: ScoresProps) {
-    const [highestScore, setHighestScore] = useState(0);
-    if (highestScore < score) {
-        setHighestScore(score);
-    }
+  const [highestScore, setHighestScore] = useState(0);
+  if (highestScore < score) {
+    setHighestScore(score);
+  }
 
-    return (
-        <div className="font-semibold text-2xl">
-            <p>Score: {score}</p>
-            <p>
-                Best Score:
-                {highestScore}
-            </p>
-        </div>
-    );
+  return (
+    <div className="font-semibold text-2xl">
+      <p>Score: {score}</p>
+      <p>Best Score: {highestScore}</p>
+    </div>
+  );
 }
